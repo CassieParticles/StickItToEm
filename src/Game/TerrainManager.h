a4d117477e2dcb72b8af31a8604e427a7c8bee51
Joseph Parsons <Joseph.Parsons2003@outlook.com>
2023-06-13 18:30:09 +0100

    Added plan for terrain generation

---
+++ src/Game/TerrainManager.h
@@ -0,0 +1,15 @@
+
+
+class TerrainManager
+{
+/*
+	Constructor-
+		Take in the size of the arena, set up scalar array, line array, and lines and triangle buffers
+	Destructor-
+		Clean up scalar array, line array, buffers
+*/
+public:
+
+protected:
+
+};
